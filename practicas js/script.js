25fe34a8748d84c4b428f3fabda0a3540766d3e6
T11641.Training14 <T11641.Training14@accenture.com>
2019-05-31 22:01:49 -0300

    Redirrecion por botones

--- practicas js/script.js
+++ practicas js/script.js
@@ -1,6 +1,7 @@
 //el body es uno de los nodos que no hace falta seleccionarlo porque ya esta incluido en el document
 //let body = document.querySelector("body");
-let btn = document.querySelector("button");
+
+/*let btn = document.querySelector("button");
 btn.addEventListener("click",hacerClick);
 
 function hacerClick()
@@ -37,6 +38,73 @@ function borrarElemento(e)
 
     document.body.removeChild(e.target.parentNode);
 }
+*/
+
+//EVENTOS POR DEFAULT: son comportamientos con los que ya vienen algunos nodos en determinados eventos
+
+let a = document.querySelector("a");
+
+a.addEventListener("click",hacerClickA);
+
+function hacerClickA(e)
+{
+    //Event.preventDefault(): Detiene el comporamietno por default de cualquier nodo.
+    
+    e.preventDefault();
+    let nuevoDiv = document.createElement("div");
+    let nuevoParrafo = document.createElement("p");
+    let botonAceptar = document.createElement("button");
+    let botonBorrar = document.createElement("button");
+
+    nuevoParrafo.innerText = "Esta seguro que quiere salir de esta pagina?";
+  //  nuevoParrafo.id = "acccept"
+    botonAceptar.innerText = "Aceptar";
+    botonBorrar.innerText = "Cancelar";
+
+    //nuevoDiv.className = "popup";
+    nuevoDiv.classList.add("popup")
+    //Nodo.classList.add()
+    //Nodo.classList.remove()
+    //Nodo.classList.toggle()
 
 
+    nuevoDiv.appendChild(nuevoParrafo);
+    nuevoDiv.appendChild(botonAceptar);
+    nuevoDiv.appendChild(botonBorrar);
+    
+    document.body.appendChild(nuevoDiv);
 
+    botonAceptar.addEventListener("click",clickAceptar);
+    botonBorrar.addEventListener("click", clickCancelar);
+
+    console.log("Click del A");
+}
+
+/*
+1) En el callback del <a> hacer la siguiente estructura:
+
+<div>
+    <p>Esta seguro que quiere salir de esta pagina?<\p>
+    <button>Aceptar<\button>
+    <button>Cancelar<\button>
+<div>
+
+2)Si se hace click en "Aceptar" se tiene que redirigir al usuario al href del <a>
+
+3)Si se hace click en "Cancelar" se tiene que borrar todo el div que contenia el boton*/
+
+
+//PARA REDIRIGIR LA VENTANA A MI LINK HREF: usar el window.location.href = a.href que seria el href del link
+function clickAceptar(e)
+{
+    window.location.href = a.href;
+
+}
+
+function clickCancelar(e)
+{
+    //Event.target: es una referencia al nodo que acaba de siparar el evento
+    //Nodo.parentNode : es una referencia al nodo padre
+
+    document.body.removeChild(e.target.parentNode);
+}
